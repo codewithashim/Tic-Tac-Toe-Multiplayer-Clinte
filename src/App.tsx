618188bfd1cb96a6633b097ad6be8f0eff52fb16
codewithashim <codewithashim@gmail.com>
2022-12-21 13:59:31 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
+type BLOCK = 'X' | 'O' | '_';
+
 function App() {
+
+  const [board, setBoard] = useState<BLOCK[]>([
+    '_', '_', '_', '_', '_', '_', '_', '_', '_'
+  ])
+  const [isXTrun, setIsXTrun] = useState<boolean>(true);
+
+  const handelClick = (index: number) => {
+    const newBoard = [...board];
+    newBoard[index] = isXTrun ? 'X' : 'O';
+    setIsXTrun(!isXTrun);
+    setBoard(newBoard);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='GameBord'>
+        <div className='Container'>
+          <div className='Row'>
+            <div className='Block' onClick={() => handelClick(0)}>
+              {board[0] !== '_' && board[0]}
+            </div>
+            <div className='Block' onClick={() => handelClick(1)}>
+              {board[1] !== '_' && board[1]}
+            </div>
+            <div className='Block' onClick={() => handelClick(2)}>
+              {board[2] !== '_' && board[2]}
+            </div>
+          </div>
+          <div className='Row'>
+            <div className='Block' onClick={() => handelClick(3)}>
+              {board[3] !== '_' && board[3]}
+            </div>
+            <div className='Block' onClick={() => handelClick(4)}>
+              {board[4] !== '_' && board[4]}
+            </div>
+            <div className='Block' onClick={() => handelClick(5)}>
+              {board[5] !== '_' && board[5]}
+            </div>
+          </div>
+          <div className='Row'>
+            <div className='Block' onClick={() => handelClick(6)}>
+              {board[6] !== '_' && board[6]}
+            </div>
+            <div className='Block' onClick={() => handelClick(7)}>
+              {board[7] !== '_' && board[7]}
+            </div>
+            <div className='Block' onClick={() => handelClick(8)}>
+              {board[8] !== '_' && board[8]}
+            </div>
+          </div>
+          <div className='commonButtonContainer'>
+            <button className='commonBtn' onClick={() => setBoard(['_', '_', '_', '_', '_', '_', '_', '_', '_'])}>Reset</button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/styles/index.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Block = styled.div`
+  aling-items: center;
+  border: 1px solid #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+`;
